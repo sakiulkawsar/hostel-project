baf24d203f99b201d7186d14d9fb6f97ba51beda
sakiulkawsar <mr.sakiulkawsar@gmail.com>
2025-12-02 23:56:14 +0600

    work in home

---
+++ rooms.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Rooms | Hosteller</title>
+        <script id="www-widgetapi-script" src="https://s.ytimg.com/yts/jsbin/www-widgetapi-vflS50iB-/www-widgetapi.js" async=""></script>
+        <script src="https://www.youtube.com/player_api"></script>
+        <link rel="stylesheet preload" as="style" href="css/preload.min.css" />
+        <link rel="stylesheet preload" as="style" href="css/icomoon.css" />
+        <link rel="stylesheet preload" as="style" href="css/libs.min.css" />
+
+        <link rel="stylesheet" href="css/rooms.min.css" />
+    </head>
+    <body>
+       <?php include_once('include/header.php') ?>
+        <header class="page">
+            <div class="container">
+                <ul class="breadcrumbs d-flex flex-wrap align-content-center">
+                    <li class="list-item">
+                        <a class="link" href="index.html">Home</a>
+                    </li>
+
+                    <li class="list-item">
+                        <a class="link" href="#">Rooms</a>
+                    </li>
+                </ul>
+                <h1 class="page_title">Rooms</h1>
+            </div>
+        </header>
+        <!-- rooms page content start -->
+        <main class="rooms section">
+            <div class="container">
+                <ul class="rooms_list">
+                    <li class="rooms_list-item" data-order="1" data-aos="fade-up">
+                        <div class="item-wrapper d-md-flex">
+                            <div class="media">
+                                <picture>
+                                    <source data-srcset="img/placeholder.jpg" srcset="img/placeholder.jpg" />
+                                    <img class="lazy" data-src="img/placeholder.jpg" src="img/placeholder.jpg" alt="media" />
+                                </picture>
+                            </div>
+                            <div class="main d-md-flex justify-content-between">
+                                <div class="main_info d-md-flex flex-column justify-content-between">
+                                    <a class="main_title h4" href="room.html">Standard Twin Room Private Shared Bathroom</a>
+                                    <p class="main_description">Diam phasellus vestibulum lorem sed risus ultricies tristique.</p>
+                                    <div class="main_amenities">
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-user icon"></i>
+                                            2 Sleeps
+                                        </span>
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-bunk_bed icon"></i>
+                                            1 bunk bed
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="main_pricing d-flex flex-column align-items-md-end justify-content-md-between">
+                                    <div class="wrapper d-flex flex-column">
+                                        <span class="main_pricing-item">
+                                            <span class="h2">$29</span>
+                                            / 1 night
+                                        </span>
+                                        <span class="main_pricing-item">
+                                            <span class="h4">$100</span>
+                                            / 7 nights
+                                        </span>
+                                    </div>
+                                    <a class="theme-element theme-element--accent btn" href="#">Book now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="rooms_list-item" data-order="2" data-aos="fade-up">
+                        <div class="item-wrapper d-md-flex">
+                            <div class="media">
+                                <picture>
+                                    <source data-srcset="img/placeholder.jpg" srcset="img/placeholder.jpg" />
+                                    <img class="lazy" data-src="img/placeholder.jpg" src="img/placeholder.jpg" alt="media" />
+                                </picture>
+                            </div>
+                            <div class="main d-md-flex justify-content-between">
+                                <div class="main_info d-md-flex flex-column justify-content-between">
+                                    <a class="main_title h4" href="room.html">Standard 6 Bed Female Dorm Shared Bathroom</a>
+                                    <p class="main_description">Diam phasellus vestibulum lorem sed risus ultricies tristique.</p>
+                                    <div class="main_amenities">
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-user icon"></i>
+                                            2 Sleeps
+                                        </span>
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-bunk_bed icon"></i>
+                                            1 bunk bed
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="main_pricing d-flex flex-column align-items-md-end justify-content-md-between">
+                                    <div class="wrapper d-flex flex-column">
+                                        <span class="main_pricing-item">
+                                            <span class="h2">$19</span>
+                                            / 1 night
+                                        </span>
+                                        <span class="main_pricing-item">
+                                            <span class="h4">$80</span>
+                                            / 7 nights
+                                        </span>
+                                    </div>
+                                    <a class="theme-element theme-element--accent btn" href="#">Book now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="rooms_list-item" data-order="3" data-aos="fade-up">
+                        <div class="item-wrapper d-md-flex">
+                            <div class="media">
+                                <picture>
+                                    <source data-srcset="img/placeholder.jpg" srcset="img/placeholder.jpg" />
+                                    <img class="lazy" data-src="img/placeholder.jpg" src="img/placeholder.jpg" alt="media" />
+                                </picture>
+                            </div>
+                            <div class="main d-md-flex justify-content-between">
+                                <div class="main_info d-md-flex flex-column justify-content-between">
+                                    <a class="main_title h4" href="room.html">Comfort Superior Double Bed Private Ensuite</a>
+                                    <p class="main_description">Diam phasellus vestibulum lorem sed risus ultricies tristique.</p>
+                                    <div class="main_amenities">
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-user icon"></i>
+                                            3 Sleeps
+                                        </span>
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-twin_bed icon"></i>
+                                            1 full bed
+                                        </span>
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-bunk_bed icon"></i>
+                                            1 twin bed
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="main_pricing d-flex flex-column align-items-md-end justify-content-md-between">
+                                    <div class="wrapper d-flex flex-column">
+                                        <span class="main_pricing-item">
+                                            <span class="h2">$99</span>
+                                            / 1 night
+                                        </span>
+                                        <span class="main_pricing-item">
+                                            <span class="h4">$220</span>
+                                            / 7 nights
+                                        </span>
+                                    </div>
+                                    <a class="theme-element theme-element--accent btn" href="#">Book now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="rooms_list-item" data-order="4" data-aos="fade-up">
+                        <div class="item-wrapper d-md-flex">
+                            <div class="media">
+                                <picture>
+                                    <source data-srcset="img/placeholder.jpg" srcset="img/placeholder.jpg" />
+                                    <img class="lazy" data-src="img/placeholder.jpg" src="img/placeholder.jpg" alt="media" />
+                                </picture>
+                            </div>
+                            <div class="main d-md-flex justify-content-between">
+                                <div class="main_info d-md-flex flex-column justify-content-between">
+                                    <a class="main_title h4" href="room.html">Standard Triple Room with Shared Bathroom</a>
+                                    <p class="main_description">Diam phasellus vestibulum lorem sed risus ultricies tristique.</p>
+                                    <div class="main_amenities">
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-user icon"></i>
+                                            2 Sleeps
+                                        </span>
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-twin_bed icon"></i>
+                                            2 twin beds
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="main_pricing d-flex flex-column align-items-md-end justify-content-md-between">
+                                    <div class="wrapper d-flex flex-column">
+                                        <span class="main_pricing-item">
+                                            <span class="h2">$29</span>
+                                            / 1 night
+                                        </span>
+                                        <span class="main_pricing-item">
+                                            <span class="h4">$115</span>
+                                            / 7 nights
+                                        </span>
+                                    </div>
+                                    <a class="theme-element theme-element--accent btn" href="#">Book now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="rooms_list-item" data-order="5" data-aos="fade-up">
+                        <div class="item-wrapper d-md-flex">
+                            <div class="media">
+                                <picture>
+                                    <source data-srcset="img/placeholder.jpg" srcset="img/placeholder.jpg" />
+                                    <img class="lazy" data-src="img/placeholder.jpg" src="img/placeholder.jpg" alt="media" />
+                                </picture>
+                            </div>
+                            <div class="main d-md-flex justify-content-between">
+                                <div class="main_info d-md-flex flex-column justify-content-between">
+                                    <a class="main_title h4" href="room.html">Standard Double Bed Private Shared Bathroom</a>
+                                    <p class="main_description">Diam phasellus vestibulum lorem sed risus ultricies tristique.</p>
+                                    <div class="main_amenities">
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-user icon"></i>
+                                            2 Sleeps
+                                        </span>
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-twin_bed icon"></i>
+                                            1 full bed
+                                        </span>
+                                        <span class="main_amenities-item d-inline-flex align-items-center">
+                                            <i class="icon-sofa icon"></i>
+                                            2 sofa beds
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="main_pricing d-flex flex-column align-items-md-end justify-content-md-between">
+                                    <div class="wrapper d-flex flex-column">
+                                        <span class="main_pricing-item">
+                                            <span class="h2">$59</span>
+                                            / 1 night
+                                        </span>
+                                        <span class="main_pricing-item">
+                                            <span class="h4">$210</span>
+                                            / 7 nights
+                                        </span>
+                                    </div>
+                                    <a class="theme-element theme-element--accent btn" href="#">Book now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="pagination d-flex align-items-center">
+                    <li class="pagination-page">
+                        <a class="pagination-page_link d-flex align-items-center justify-content-center" href="#" data-current="true">1</a>
+                    </li>
+                    <li class="pagination-page">
+                        <a class="pagination-page_link d-flex align-items-center justify-content-center" href="#">2</a>
+                    </li>
+                    <li class="pagination-page">
+                        <a class="pagination-page_link d-flex align-items-center justify-content-center" href="#">3</a>
+                    </li>
+                </ul>
+            </div>
+        </main>
+        <!-- rooms page content end -->
+        <?php include_once('include/footer.php') ?>
+        <script src="js/common.min.js"></script>
+    </body>
+</html>
